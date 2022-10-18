d0e587182c29de7304990a33e715e18cd274f87d
tehjul <sibillej@gmail.com>
2022-10-18 23:32:44 +0200

    Add truffle exercices

---
+++ 4. Truffle & CI-CD/Mumbai/truffle-config.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+const { MNEMONIC, MUMBAI_ID, MUMBAI_URL } = process.env;
+
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+module.exports = {
+
+  networks: {
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+    },
+    mumbai: {
+      provider: function() {return new HDWalletProvider({mnemonic:{phrase:MNEMONIC}, providerOrUrl:MUMBAI_URL+MUMBAI_ID})},
+      network_id: 80001
+    }
+  },
+
+  // Set default mocha options here, use special reporters, etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.8.13" // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+      //  optimizer: {
+      //    enabled: false,
+      //    runs: 200
+      //  },
+      //  evmVersion: "byzantium"
+      // }
+    }
+  }
+};
