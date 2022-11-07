5eeab015cf456a44ac9e7c40ccf5bee54bd60a6e
tehjul <sibillej@gmail.com>
2022-11-07 16:45:28 +0100

    add events

--- 6. Dapp/simple-storage-react/client/src/components/Demo/Contract.jsx
+++ 6. Dapp/simple-storage-react/client/src/components/Demo/Contract.jsx
@@ -1,17 +1,44 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
+import useEth from "../../contexts/EthContext/useEth";
 
 function Contract({ value, greeter }) {
   const spanEle = useRef(null);
+  const [eventValue, setEventValue] = useState("");
+  const [oldEvents, setOldEvents] = useState("");
+
+  const { state: { contract } } = useEth();
 
   useEffect(() => {
+    async function getEvents() {
+      let oldEvents = await contract.getPastEvents('valueChanged', {
+        fromBlock: 0,
+        toBlock: 'latest'
+      });
+      let oldies = [];
+      oldEvents.forEach(event => {
+        oldies.push(event.returnValues._val + ",");
+      });
+      setOldEvents(oldies);
+
+      await contract.events.valueChanged({ fromBlock: "earliest" })
+        .on('data', event => {
+          let lesevents = event.returnValues._val;
+          setEventValue(lesevents);
+        })
+        .on('changed', changed => console.log(changed))
+        .on('error', err => console.log(err))
+        .on('connected', str => console.log(str))
+    }
     spanEle.current.classList.add("flash");
     const flash = setTimeout(() => {
       spanEle.current.classList.remove("flash");
     }, 300);
     return () => {
       clearTimeout(flash);
+      getEvents();
     };
-  }, [value]);
+
+  }, [value, contract]);
 
   return (
     <code>
@@ -46,7 +73,10 @@ function Contract({ value, greeter }) {
   function setGreet(string calldata _newGreet) public {
     greeter = _newGreet;
   }
-}`}
+}
+Events arriving: `} {eventValue} {`
+
+Old events: `} {oldEvents}
     </code>
   );
 }
--- 6. Dapp/simple-storage-react/truffle/contracts/SimpleStorage.sol
+++ 6. Dapp/simple-storage-react/truffle/contracts/SimpleStorage.sol
@@ -5,12 +5,15 @@ contract SimpleStorage {
   uint256 value;
   string greeter;
 
+  event valueChanged(uint _val);
+
   function read() public view returns (uint256) {
     return value;
   }
 
   function write(uint256 newValue) public {
     value = newValue;
+    emit valueChanged(newValue);
   }
 
   function greet() public view returns (string memory) {
