1c08d15e73412df1ac701544dfe260a830f443cb
tehjul <sibillej@gmail.com>
2022-11-13 18:09:18 +0100

    Add exercice to mint and transfer tokens

---
+++ 6. Dapp/exercice/client/src/App.jsx
@@ -0,0 +1,17 @@
+import { EthProvider } from "./contexts/EthContext";
+import Button from "./components/Button";
+import "./App.css";
+
+function App() {
+  return (
+    <EthProvider>
+      <div id="App" >
+        <div className="container">
+          <Button />
+        </div>
+      </div>
+    </EthProvider>
+  );
+}
+
+export default App;
