3a6f453ce49d9128d7732e33f5358d665e0f1794
tehjul <sibillej@gmail.com>
2022-11-26 20:05:07 +0100

    add pinata script

---
+++ 8. NFT/coursIpfs/scriptpinata.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const key = process.env.PINATA_KEY;
+const secret = process.env.PINATA_SECRET;
+const pinataSDK = require('@pinata/sdk');
+const pinata = new pinataSDK(key, secret);
+const fs = require('fs');
+const readableStreamForFile = fs.createReadStream('logo.png');
+
+const options = {
+    pinataMetadata: {
+        name: "AlyraNFT",
+    },
+    pinataOptions: {
+        cidVersion: 0
+    }
+};
+
+
+pinata.pinFileToIPFS(readableStreamForFile, options).then((result) => {
+    const body = {
+        description: "Un NFT tres beau pour alyra.",
+        image: result.IpfsHash,
+        name: "BestNFT",
+    };
+
+    pinata.pinJSONToIPFS(body, options).then((json) => {
+        console.log(json);
+    }).catch((err) => {
+        console.log(err);
+    });
+
+}).catch((err) => {
+    console.log(err);
+});
